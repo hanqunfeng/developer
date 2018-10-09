660099fe4d647668bae7b8a8d7061b842c6af3cb
hanqf <hanqf2008@163.com>
2018-10-09 16:56:10 +0800

    初始化sql

--- db_sql/cp2015db.sql
+++ db_sql/cp2015db.sql
@@ -1,5 +1,15 @@
 
 
+
+--
+-- Current Database: `cp2015db`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `cp2015db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `cp2015db`;
+
+
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
